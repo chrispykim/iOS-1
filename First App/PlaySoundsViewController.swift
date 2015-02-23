e2cb2c139ca4e4f237c3d6b90bfe733cb42d2190
Chris Kim <chrisKim@Chriss-MacBook-Air.local>
2015-02-22 22:28:31 -0800

    version 1.0

---
+++ First App/PlaySoundsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaySoundsViewController.swift
+//  First App
+//
+//  Created by Chris Kim on 12/26/14.
+//  Copyright (c) 2014 Chris Kim. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ First App/RecordedAudio.swift
@@ -0,0 +1,9 @@
+//
+//  RecordedAudio.swift
+//  First App
+//
+//  Created by Chris Kim on 12/27/14.
+//  Copyright (c) 2014 Chris Kim. All rights reserved.
+//
+
+import Foundation
